9faecd76de2d78e71c6caf08d70c05a0f7882618
Edgar Jesus <mendesbr92@gmail.com>
2024-03-19 21:55:46 -0300

    closes #3 - javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+function changeTrafficLight() {
+    
+       if(document.getElementById("yellow").className === "light yellow-light"){
+              document.getElementById("red").className = "light red-light";
+              document.getElementById("yellow").className = "light light-off"
+       }
+       else {
+              if(document.getElementById("red").className === "light red-light"){
+                     document.getElementById("red").className = "light light-off";
+                     document.getElementById("green").className = "light green-light";
+              }
+
+              else {
+              
+                     if(document.getElementById("green").className === "light green-light"){
+                            document.getElementById("yellow").className = "light yellow-light";
+                            document.getElementById("green").className = "light light-off";
+                     }
+              }
+       }
+       
+}
